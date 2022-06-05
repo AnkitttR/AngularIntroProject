3d1cda6c6e6ae57796894d97f3f95fa2f8602a9a
Ankit <ankitthakur1998@gmail.com>
2022-06-05 18:16:37 +0530

    Routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EmailComponent } from './email/email.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"sendemail",
+    component:EmailComponent,
+    pathMatch:"full"
+  }
+  ,
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
